819c892d737b6444677ad82235d74d26e5c3e69c
Yan Svintsov <svintsoff.ya@yandex.ru>
2024-05-31 20:32:47 +0400

    Fix bug

--- src/controller/HelloController.php
+++ src/controller/HelloController.php
@@ -2,35 +2,23 @@
 
 namespace Revamp\App\Controller;
 
+use Revamp\App\Request\Hello\HelloRequest;
+use Revamp\App\Response\Hello\HelloResponse;
 use Revamp\Core\Types\Bruno\Bruno;
 use Revamp\Core\Types\Request\Request;
 use Revamp\Core\Types\Response\Response;
 use Revamp\Core\Types\Route\Route;
 use Revamp\Core\Types\Template\Controller\ControllerTemplate;
-use Revamp\Core\User\Request\AuthorizeUserRequest;
-use Revamp\Core\User\Request\CreateUserRequest;
-use Revamp\Core\User\Response\AuthorizeUserResponse;
-use Revamp\Core\User\Response\CreateUserResponse;
-use Revamp\Core\User\UserHandlerInterface;
 
 #[Bruno(name: 'User')]
 class HelloController extends ControllerTemplate
 {
     #[Route(uri: '/hello', methods: ['POST'])]
-    #[Request(requestTemplate: CreateUserRequest::class)]
-    #[Response(responseTemplate: CreateUserResponse::class)]
-    #[Bruno(name: 'Create User')]
-    public function register(): void
+    #[Request(requestTemplate: HelloRequest::class)]
+    #[Response(responseTemplate: HelloResponse::class)]
+    #[Bruno(name: 'Send Hello')]
+    public function sendHello(): void
     {
-
-    }
-
-    #[Route(uri: '/authorize', methods: ['POST'])]
-    #[Request(requestTemplate: AuthorizeUserRequest::class)]
-    #[Response(responseTemplate: AuthorizeUserResponse::class)]
-    #[Bruno(name: 'Authorize User')]
-    public function authorize(UserHandlerInterface $user): void
-    {
-       $user->authorize($this->request->login, $this->request->password);
+        $this->response->data = "Hello, {$this->request->name}";
     }
 }
